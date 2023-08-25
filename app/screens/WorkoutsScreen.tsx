0291fa1d2fb20b48116394dd9e7ee4332e5d2708
sam <samtoussi@yahoo.ca>
2023-08-24 20:55:05 -0400

    implement set and rep logic

--- app/screens/WorkoutsScreen.tsx
+++ app/screens/WorkoutsScreen.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import { StyleSheet, FlatList } from "react-native";
-import TargetMuscles from "../components/TargetMuscles";
+import { StyleSheet, FlatList, View, Image, Text } from "react-native";
 import Screen from "../components/Screen";
 import WorkoutItem from "../components/WorkoutItem";
+import theme from "../config/theme";
+
+interface WorkoutSet {
+  reps: number;
+  actualReps: number;
+}
 
 interface Workout {
   id: number;
   title: string;
   imageUrl: number;
+  frontImageUrl?: number;
+  sets: WorkoutSet[];
 }
 
 const WorkoutsScreen: React.FC = () => {
   const handleButtonPress = (workoutId: number) => {};
+  const renderSeparator = () => <View style={styles.separator} />;
 
   const renderItem = ({ item }: { item: Workout }) => {
     if (item.id === 0) {
-      return <TargetMuscles workout={item} onButtonPress={handleButtonPress} />;
+      return (
+        <View style={styles.targetMuscleImageContainer}>
+          <Image source={item.imageUrl} style={styles.image} />
+          <Image
+            source={item.frontImageUrl ?? item.imageUrl}
+            style={styles.image}
+          />
+        </View>
+      );
     } else {
       return <WorkoutItem workout={item} onButtonPress={handleButtonPress} />;
     }
@@ -27,6 +43,7 @@ const WorkoutsScreen: React.FC = () => {
         data={workouts}
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderItem}
+        ItemSeparatorComponent={renderSeparator}
       />
     </Screen>
   );
@@ -34,27 +51,58 @@ const WorkoutsScreen: React.FC = () => {
 
 export default WorkoutsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    marginHorizontal: 20,
+    backgroundColor: theme.colors.grey0,
+  },
+  image: { height: 300, width: 150, resizeMode: "contain" },
+  targetMuscleImageContainer: {
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
 
 const workouts = [
   {
     id: 0,
-    title: "Featured Workout",
+    title: "Target Muscles",
     imageUrl: require("../../assets/images/targetMuscles/backFull.png"),
+    frontImageUrl: require("../../assets/images/targetMuscles/frontFull.png"),
+    sets: [],
   },
   {
     id: 1,
-    title: "Workout 1",
+    title: "Deadlift",
     imageUrl: require("../../assets/images/exercises/romanianDeadlift.png"),
+    sets: [
+      { reps: 8, actualReps: 0 },
+      { reps: 8, actualReps: 0 },
+      { reps: 6, actualReps: 0 },
+    ],
   },
   {
     id: 2,
-    title: "Workout 1",
+    title: "Shrugs",
     imageUrl: require("../../assets/images/exercises/shrugs.png"),
+    sets: [
+      { reps: 5, actualReps: 0 },
+      { reps: 5, actualReps: 0 },
+      { reps: 5, actualReps: 0 },
+      { reps: 5, actualReps: 0 },
+    ],
   },
   {
     id: 3,
-    title: "Workout 1",
+    title: "Suqats",
     imageUrl: require("../../assets/images/exercises/squat.png"),
+    sets: [
+      { reps: 8, actualReps: 0 },
+      { reps: 8, actualReps: 0 },
+      { reps: 8, actualReps: 0 },
+      { reps: 6, actualReps: 0 },
+    ],
   },
 ];
